201e085805da605ebb3428053bc515ec2972ba8d
Mason Stigall <masonstigall@gmail.com>
2018-06-14 13:25:38 -0400

    giphy api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,31 @@
-var apiKey = "6vmGWZlfm1G2OE9sIxi1eRx9Xv3WaHok";
+$(document).ready(function() {
+//variables for api key, url and starting button array
+var apiKey = "apikey=6vmGWZlfm1G2OE9sIxi1eRx9Xv3WaHok";
+var queryURL = "https://api.giphy.com";
+var instruments = ["guitar", "piano", "drums", "trumpet"];
+
+// Function for displaying initial buttons
+function renderButtons() {
+    for (i = 0; i < instruments.length; i++) {
+        $("<button>").text(instruments[i]).appendTo("#gif-buttons");
+        console.log(instruments[i]);
+    }
+}
+renderButtons();
+
+//capturing clicks from navbar to add new button
+$("#add-button").on("click", function (event) {
+    event.preventDefault();
+    var newItem = $("#user-input").val();
+    $("<button>").text(newItem).addClass("item-button").appendTo($("#gif-buttons"));
+});
+
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+});
+
+$("#input-form").on("submit", function(event){
+    event.preventDefault();
+});
+
+});
